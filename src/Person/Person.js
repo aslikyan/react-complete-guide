489f3240c12911bf9dc64631fe9f2f770034627f
aslikyan <arthur.aslikyan@bigbrain.info>
2019-03-06 02:33:25 +0400

    Used props to crested

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <h2>Hi I'm a Person</h2>
+const person = (props) => {
+    return(
+        <div>
+        <h2>Hi I'm a {props.name}, and I'm a {props.age} years old</h2>
+        <h3>And {props.children}</h3>
+        </div>
+    )
 }
 
 export default person;
