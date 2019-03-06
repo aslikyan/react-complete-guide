acc83537137718fe017b59291217666f7a5fef8b
aslikyan <arthuraslikyan3@gmail.com>
2019-03-06 21:07:30 +0400

    In previous commit we managed state and used this.setState, but what we do when we want manage manipulate state in function based component.In react 18.6 we have hooks, and useState hook.In this commit used useState hook as function that returns two parameters in array and assigned it two array with two elements personsState and setPersonsState(ES6 object destructuring).First element returns initial state and second as function wants parameter to set state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Arthur', age: '22'},
       {name: 'Annie', age: '25'},
       {name: 'Leo', age: '12'}
-    ]
-  }
-  switchNameHandler = () => {
-    this.setState({
+    ],
+  })
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: 'John', age: '12'},
         {name: 'Juliana', age: '14'},
         {name: 'Sophie', age: '13'}
-      ]
-    });
+      ],
+    })
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi components who are you?</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobby is racing</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi components who are you?</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobby is racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  )
 }
 
-export default App;
+export default app
